7715bfe5cf7951da691dbedae9161800c0b64d08
Elanthamizh2606 <e.elanthamizh2606@gmail.com>
2022-08-25 17:30:40 +0530

    First Push

---
+++ src/com/git/Sample.java
@@ -0,0 +1,20 @@
+package com.git;
+
+public class Sample {
+	private void meth1() {
+		System.out.println("1");
+	}
+	
+	private void meth2() {
+		System.out.println("2");
+	}
+	
+	private void meth3() {
+		System.out.println("3");
+	}
+	
+	private void meth4() {
+		System.out.println("4");
+	}
+
+}
